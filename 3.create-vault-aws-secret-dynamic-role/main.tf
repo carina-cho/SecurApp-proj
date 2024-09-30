e2a16282b9a8980d3e42a9358867be1a265e3321
carina-cho <chowai4@gmail.com>
2024-09-30 16:19:45 +0800

    update main..tf file

--- 3.create-vault-aws-secret-dynamic-role/main.tf
+++ 3.create-vault-aws-secret-dynamic-role/main.tf
@@ -20,18 +20,18 @@ resource "vault_aws_secret_backend" "aws" {
   default_lease_ttl_seconds = 600
   max_lease_ttl_seconds = 1800
   lifecycle {
-    ignore_changes = [ 
-        access_key, secret_key
-     ]
+    ignore_changes = [
+    access_key, secret_key
+    ]
   }
 }
 
 resource "vault_aws_secret_backend_role" "iam_admin_dynamic_role" {
-    backend = vault_aws_secret_backend.aws.path
-    name    = var.secret_role_name.master_iamadmin_role_name
-    credential_type = var.credential_type.iam_user
-    policy_arns = var.policy_arns_name.iamadmin
-      policy_document = <<EOT
+  backend = vault_aws_secret_backend.aws.path
+  name = var.secret_role_name.master_iamadmin_role_name
+  credential_type = var.credential_type.iam_user
+  policy_arns = var.policy_arns_name.iamadmin
+  policy_document = <<EOT
 {
   "Version": "2012-10-17",
   "Statement": [
@@ -46,22 +46,60 @@ EOT
 }
 
 
-resource "vault_aws_secret_backend_role" "network_admin_dynamic_role" {
-    backend = vault_aws_secret_backend.aws.path
-    name = var.secret_role_name.master_netadmin_role_name
-    credential_type = var.credential_type.iam_user
-    policy_arns = var.policy_arns_name.networkadmin
-    policy_document = <<EOT
-    {
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": "iam:GetUser",
-      "Resource": "arn:aws:iam::034362076599:user/vault-token-terraform-*"
-    }
-  ]
-}
-EOT
-    }
+# resource "vault_aws_secret_backend_role" "network_admin_dynamic_role" {
+#   backend = vault_aws_secret_backend.aws.path
+#   name = var.secret_role_name.master_netadmin_role_name
+#   credential_type = var.credential_type.iam_user
+#   policy_arns = var.policy_arns_name.networkadmin
+#   policy_document = <<EOT
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Effect": "Allow",
+#       "Action": "iam:GetUser",
+#       "Resource": "arn:aws:iam::339713018668:user/vault-token-terraform-*"
+#     }
+#   ]
+# }
+# EOT
+# }
+
+# resource "vault_aws_secret_backend_role" "ec2_admin_dynamic_role" {
+#   backend = vault_aws_secret_backend.aws.path
+#   name = var.secret_role_name.master_ec2admin_role_name
+#   credential_type = var.credential_type.iam_user
+#   policy_arns = var.policy_arns_name.ec2admin
+#   policy_document = <<EOT
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Effect": "Allow",
+#       "Action": "iam:GetUser",
+#       "Resource": "arn:aws:iam::339713018668:user/vault-token-terraform-*"
+#     }
+#   ]
+# }
+# EOT
+# }
+
+# resource "vault_aws_secret_backend_role" "rds_admin_dynamic_role" {
+#   backend = vault_aws_secret_backend.aws.path
+#   name = var.secret_role_name.master_rdsadmin_role_name
+#   credential_type = var.credential_type.iam_user
+#   policy_arns = var.policy_arns_name.rdsadmin
+#   policy_document = <<EOT
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Effect": "Allow",
+#       "Action": "iam:GetUser",
+#       "Resource": "arn:aws:iam::*:user/vault-token-terraform-*"
+#     }
+#   ]
+# }
+# EOT
+# }
 
